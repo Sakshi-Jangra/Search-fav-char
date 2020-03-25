3d6a429092bac35888f0947cc9bc3627184413b6
Sakshi Jangra <sakshijangra111000@gmail.com>
2020-03-26 01:25:30 +0530

    Resolving some errors

--- app.js
+++ app.js
@@ -1,43 +1,24 @@
 const charactersList = document.getElementById('charactersList');
-
-let hpCharacters = []; // reference to all the harry potter charcters
-
 const searchBar = document.getElementById('searchBar');
-console.log(searchBar);
-
-// get search string in input of html , use to search through data , make API request to backend,
-// get filtered data
-
-// filter and map functions array functions that can be used in JS
+let hpCharacters = [];
 
 searchBar.addEventListener('keyup', e => {
   const searchString = e.target.value.toLowerCase();
-  console.log(searchString);
-
-  // if search String contains capital letters ----> small letters also
-  // if search str is small -- > small
-  // convert name to lower case & cmpare
-  // convert house to lowercase & compare
 
   const filteredCharacters = hpCharacters.filter(character => {
     return (
-      // if the characters is in the name or house then return it
       character.name.toLowerCase().includes(searchString) ||
       character.house.toLowerCase().includes(searchString)
     );
   });
-
   displayCharacters(filteredCharacters);
 });
 
-// loads all charachters from site
-
-const loadCharachters = async () => {
+const loadCharacters = async () => {
   try {
     const res = await fetch('https://hp-api.herokuapp.com/api/characters');
     hpCharacters = await res.json();
     displayCharacters(hpCharacters);
-    console.log(hpCharacters);
   } catch (err) {
     console.error(err);
   }
@@ -47,14 +28,15 @@ const displayCharacters = characters => {
   const htmlString = characters
     .map(character => {
       return `
-      <li class="character">
-          <h2>${character.name}</h2>
-          <p>House: ${character.house}</p>
-          <img src="${character.image}"></img>
-      </li>
-  `;
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <img src="${character.image}"></img>
+            </li>
+        `;
     })
     .join('');
   charactersList.innerHTML = htmlString;
 };
-loadCharachters();
+
+loadCharacters();
